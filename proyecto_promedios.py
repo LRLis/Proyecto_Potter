f3f41f7fdd1adc367897defc02ddb4ca20b7d569
LRLis <94798863+LRLis@users.noreply.github.com>
2022-08-26 15:27:32 -0500

    Add files via upload

---
+++ proyecto_promedios.py
@@ -0,0 +1,9 @@
+print("Escribe las calificaciones usando escala de 0 a 100")
+print("Escribe el valor de una actividad")
+va=int(input())
+print("Escribe cuanto sacaste en esa actividad")
+sa=int(input())
+
+cp=(sa*va)/100
+
+print("El porcentaje obtenido es:", cp)
