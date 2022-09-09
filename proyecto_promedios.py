44ca2e611712b17c95091d67a366966d5be12391
LRLis <94798863+LRLis@users.noreply.github.com>
2022-09-09 10:01:52 -0500

    Update proyecto_promedios.py

--- proyecto_promedios.py
+++ proyecto_promedios.py
@@ -28,3 +28,29 @@ print("Total de números ingresados: ", c)
 print("Promedio: ",sum/c)
 
 print("=================================================================")
+
+print("Pronostico de calificaiones")
+print("Coloca cuantas calificaciones son: ")
+so=float(input())
+print("Calificaciones que tienes: ")
+ob=float(input())
+
+a=so-ob
+
+print("Coloca las calificaiones que tienes: ")
+print("Se detiene al colocar un número negativo, y no se toma en cuenta para el promedio")
+
+n=0
+c=0
+sum=0
+while n>=0:
+    n=float(input("Pon un número: "))
+    if n>0:
+        c=c+1
+        sum=sum+n
+
+print("Total de números ingresados: ", c)
+print("Promedio de las calificaiones sacando 50 en actividades restantes: ",(sum+a*50)/so)
+print("Promedio de las calificaiones sacando 100 en actividades restantes: ",(sum+a*100)/so)
+
+print("=================================================================")
