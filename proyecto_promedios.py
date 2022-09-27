25a2e3867674611d6d167716081120a8edfa93c7
LRLis <94798863+LRLis@users.noreply.github.com>
2022-09-27 10:00:24 -0500

    Update proyecto_promedios.py

--- proyecto_promedios.py
+++ proyecto_promedios.py
@@ -1,14 +1,15 @@
-print("Seleciona una opción")
+def saludo():
+    return "Hola bienvenido \nSeleciona una opción"
+bienvenida=saludo()
+print(bienvenida)
+
 print("1)Obtener porcentaje sacado en una actividad")
 print("2)Obtener promedio de calificaciones")
 print("3)Predecir promedio sacando 50 y 100 en actividades faltantes")
-
 op=int(input())
 
 if op == 1:
     print("Porcentaje obtenido en una activida")
-    print("Escribe las calificaciones usando escala de 0 a 100")
-    print("Calificaión obtenida por porcentaje de valor")
     print("Escribe el valor de una actividad")
     va=float(input())
     print("Escribe cuanto sacaste en esa actividad")
@@ -18,40 +19,50 @@ if op == 1:
     
 if op == 2:
     print("Promedio de calificaiones")
-    print("Se detiene al colocar un número negativo, y no se toma en cuenta para el promedio")
-    n=0
-    c=0
-    sum=0
-    while n>=0:
-        n=float(input("Pon un número: "))
-        if n>0:
-            c=c+1
-            sum=sum+n
-    print("Total de números ingresados: ", c)
-    print("Promedio: ",sum/c)
+    calificaciones = int(input("Ingrese la cantidad de calificaciones: "))
+    lista = []
+    posicion = 1
+    totalIngresadas = calificaciones
+ 
+    while calificaciones > 0:
+        valor = float(input("Ingrese calificación {}: ".format(posicion)))
+        lista.append(valor)
+        calificaciones -= 1
+        posicion += 1
+ 
+    print(lista)
+ 
+    sumaNotas = 0
+    for i in lista:
+        sumaNotas = sumaNotas + i
+    promedio = round(sumaNotas/totalIngresadas,2)
+    print("El promedio de las notas ingresadas es: {}".format(promedio))
 
 if op == 3:
     print("Pronostico de calificaiones")
     print("Coloca cuantas calificaciones son: ")
     so=float(input())
-    print("Calificaciones que tienes: ")
-    ob=float(input())
-    a=so-ob
-    print("Coloca las calificaiones que tienes: ")
-    print("Se detiene al colocar un número negativo, y no se toma en cuenta para el promedio")
-    n=0
-    c=0
-    sum=0
-    while n>=0:
-        n=float(input("Pon un número: "))
-        if n>0:
-            c=c+1
-            sum=sum+n
-    print("Total de números ingresados: ", c)
-    print("Promedio de las calificaiones sacando 50 en actividades restantes: ",(sum+a*50)/so)
-    print("Promedio de las calificaiones sacando 100 en actividades restantes: ",(sum+a*100)/so)
-
+    calificaciones = int(input("Ingrese la cantidad de calificaciones que tienes: "))
+    lista = []
+    posicion = 1
+    totalIngresadas = calificaciones
+    a1=(so-calificaciones)*50
+    a2=(so-calificaciones)*100
+    while calificaciones > 0:
+        valor = float(input("Ingrese calificación {}: ".format(posicion)))
+        lista.append(valor)
+        calificaciones -= 1
+        posicion += 1
+ 
+    print(lista)
+ 
+    sumaNotas = 0
+    for i in lista:
+        sumaNotas = sumaNotas + i
+    promedio1 = round((sumaNotas+a1)/so,2)
+    promedio2 = round((sumaNotas+a2)/so,2)
+    print("El promedio sacando 50 en las calificaciones faltantes: {}".format(promedio1))
+    print("El promedio sacando 100 en las calificaiones faltantes: {}".format(promedio2))
+    
 if op > 3:
     print("No existe esta opción")
-    
-
